f546e0605d8540f4061ebd16d10a8344b6a743ba
Adrian Fulladolsa <ful21592@uvg.edu.gt>
2023-09-20 21:58:36 -0600

    Cambios finales
    Modificaciones a iluminacion, posicion de elementos y especularidad de materiales

--- Main.py
+++ Main.py
@@ -17,8 +17,8 @@ from Lib.figure import *
 from Lib.lights import *
 from Lib.materials import *
 
-width = 256
-height = 256
+width = 500
+height = 500
 
 pygame.init()
 
@@ -28,42 +28,43 @@ screen.set_alpha(None)
 raytracer = Raytracer(screen)
 raytracer.rtClearColor(0.2109,0.30078,0.42578)
 
-boton = Material(diffuse=(0.02734,0.02734,0.0351), spec = 5, ks = 0.02)
-nariz = Material(diffuse=(0.9884,0.2305,0.2304), spec = 5, ks = 0.02)
-cuerpo = Material(diffuse=(0.941,0.8867,0.789), spec = 5, ks = 0.02)
-boca = Material(diffuse=(0.3594,0.2969,0.289), spec = 5, ks = 0.02)
+boton = Material(diffuse=(0.02734,0.02734,0.0351), spec = 8, ks = 0.02)
+nariz = Material(diffuse=(0.9884,0.2305,0.2304), spec = 6, ks = 0.02)
+cuerpo = Material(diffuse=(1,1,1), spec = 3, ks = 0.02)
+boca = Material(diffuse=(0.3594,0.2969,0.289), spec = 6, ks = 0.02)
 ojo = Material(diffuse=(0.8398,0.7656,0.8008), spec = 9, ks = 0.02)
 
-#Nieve
-raytracer.scene.append(Sphere(position=(0,-1.2,-5), radius = 1.2, material = cuerpo))
-raytracer.scene.append(Sphere(position=(0,0.55,-5), radius = 0.95, material = cuerpo))
-raytracer.scene.append(Sphere(position=(0,1.94,-5), radius = 0.7, material = cuerpo))
+objetos = [
+            Sphere(position=(0,-1.2,-5), radius = 1.2, material = cuerpo),
+            Sphere(position=(0,0.55,-5), radius = 0.95, material = cuerpo),
+            Sphere(position=(0,1.94,-5), radius = 0.7, material = cuerpo),
+            Sphere(position=(0.145,2.04,-4.1), radius = 0.07, material = ojo),
+            Sphere(position=(-0.145,2.04,-4.1), radius = 0.07, material = ojo),
+            Sphere(position=(-0.142,2,-3.95), radius = 0.03, material = boton),
+            Sphere(position=(0.147,2,-3.95), radius = 0.03, material = boton),
+            Sphere(position=(0,1.75,-4), radius = 0.17, material = nariz),
+            Sphere(position=(-0.25,1.54,-4.15), radius = 0.05, material = boca),
+            Sphere(position=(-0.10,1.49,-4.15), radius = 0.05, material = boca),
+            Sphere(position=(0.10,1.51,-4.15), radius = 0.05, material = boca),
+            Sphere(position=(0.25,1.55,-4.15), radius = 0.05, material = boca),
+            Sphere(position=(0,-1,-4), radius = 0.35, material = boton),
+            Sphere(position=(0,0.15,-4), radius = 0.25, material = boton),
+            Sphere(position=(0,1,-4), radius = 0.15, material = boton)
+          ]
 
-#Ojos
-raytracer.scene.append(Sphere(position=(0.145,2.04,-4.1), radius = 0.05, material = ojo))
-raytracer.scene.append(Sphere(position=(-0.145,2.04,-4.1), radius = 0.05, material = ojo))
-raytracer.scene.append(Sphere(position=(-0.145,1.98,-3.95), radius = 0.025, material = boton))
-raytracer.scene.append(Sphere(position=(0.145,1.98,-3.95), radius = 0.025, material = boton))
+luces = [
+    AmbientLight(intensity=0.05),
+    DirectionalLight(direction=(0,1.15,-2), intensity=0.50),
+    DirectionalLight(direction=(0,0.5,-1), intensity=0.20),
+    PointLight(point=(2.5,0,-5), intensity=1, color= (0.1,0.2,1)),
+    PointLight(point=(-2,-0.3,-5), intensity=1, color= (1,0,1))
+]
 
-#Nariz
-raytracer.scene.append(Sphere(position=(0,1.75,-4), radius = 0.17, material = nariz))
+for objeto in objetos:
+    raytracer.scene.append(objeto)
 
-#Boca
-raytracer.scene.append(Sphere(position=(-0.35,1.54,-4.15), radius = 0.075, material = boca))
-raytracer.scene.append(Sphere(position=(-0.13,1.49,-4.15), radius = 0.075, material = boca))
-raytracer.scene.append(Sphere(position=(0.12,1.51,-4.15), radius = 0.075, material = boca))
-raytracer.scene.append(Sphere(position=(0.35,1.55,-4.15), radius = 0.075, material = boca))
-
-#Botones
-raytracer.scene.append(Sphere(position=(0,-1,-4), radius = 0.35, material = boton))
-raytracer.scene.append(Sphere(position=(0,0.15,-4), radius = 0.25, material = boton))
-raytracer.scene.append(Sphere(position=(0,1,-4), radius = 0.15, material = boton))
-
-#Luz
-raytracer.lights.append(AmbientLight(intensity=0.05))
-raytracer.lights.append(DirectionalLight(direction=(0.5,-1,-2), intensity=0.90))
-raytracer.lights.append(PointLight(point=(2.5,0,-5), intensity=1, color= (0.8,0.9,1)))
-
+for luz in luces:
+    raytracer.lights.append(luz)
 
 isRunning = True
 while isRunning:
