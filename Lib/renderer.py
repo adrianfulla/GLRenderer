9e231d4613f9787f95a4be63442a9ecbd3a5d25b
Adrian Fulladolsa <84196543+adrianfulla@users.noreply.github.com>
2023-11-02 22:17:48 -0600

    Subida de archivos de libreria

---
+++ Lib/renderer.py
@@ -0,0 +1,13 @@
+"""
+ Universidad del Valle de Guatemala
+  Facultad de Ingenieria
+  Departamento de Ciencia de la Computacion.
+  Graficas por Computadora.
+  Sección: 20
+
+  OGL1: OGL1: 3D Models & Transforms
+
+  @version 1.0
+  @author Adrian Fulladolsa Palma | Carne 21592
+"""
+
